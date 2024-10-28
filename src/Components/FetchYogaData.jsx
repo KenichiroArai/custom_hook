d5bae2ac6b4f093b8a1bd3610a789dd182182c0e
KenichiroArai <sammyleejunior@gmail.com>
2024-10-28 01:30:34 -0400

    練習問題

---
+++ src/Components/FetchYogaData.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import UseFetch from './UseFetch';
+
+const FetchYogaData = () => {
+    
+    const [data]=UseFetch('https://api.npoint.io/4459a9a10e43812e1152');
+    console.log(data);
+
+    return (
+    <>
+
+    </>
+    )
+}
+export default FetchYogaData
